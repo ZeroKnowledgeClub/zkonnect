f27f8d04d4c9dc90b0c2990e144ca01fcfcb4cf3
Arshdeep54 <balarsh535@gmail.com>
2024-07-31 13:38:12 +0530

    updates contract

--- contracts/posts.sol
+++ contracts/posts.sol
@@ -1,66 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
-
-contract MappingPubkey {
-    mapping (address => string) private pubKeyToDisplayName;
-
-    function setpubKeyToDisplayName( string memory displayName ) public {
-        pubKeyToDisplayName[msg.sender] = displayName; //Address of the account that called mapped to its display name
-    }
-    
-    function getDisplayName(address user) public view returns ( string memory ){
-        return pubKeyToDisplayName[user];
-    }
-} 
-
-interface IMappingPubkey {
-    function getDisplayName(address user) external view returns ( string memory );
-}
-
 contract Posts {
-    struct Post{
-        uint postIndex; // Unique Identification of Post
-        uint postTime;
+    mapping(string => string) private pubKeyToDisplayName;
+
+    function setpubKeyToDisplayName(
+        string memory displayName,
+        string memory public_key
+    ) public {
+        require(bytes(displayName).length > 0, "Display name not set");
+        pubKeyToDisplayName[public_key] = displayName; //Address of the account that called mapped to its display name
+    }
+
+    function getDisplayName(string memory public_key)
+        public
+        view
+        returns (string memory)
+    {
+        return pubKeyToDisplayName[public_key];
+    }
+
+    struct Post {
+        uint256 postIndex; // Unique Identification of Post
+        uint256 postTime;
         string CIDHash;
-        uint Reward; //Initialises with value 0
+        uint256 Reward; //Initialises with value 0
         //string flair; What else can be added?
     }
-    mapping (string => Post[]) private userPosts;
+    mapping(string => Post[]) private userPosts;
+    mapping(uint256 => string) private PostToUser;
+
     Post[] public allPosts;
 
-    constructor(address _mappingPubkeyAddress) {
-        mappingPubkeyContract = IMappingPubkey(_mappingPubkeyAddress);
-    }
-    IMappingPubkey private mappingPubkeyContract;
-    
-    function createPost(Post memory post) public {
-        string memory displayName = mappingPubkeyContract.getDisplayName(msg.sender);
-        require(bytes(displayName).length > 0, "Display name not set");
+    // mapping(uint256 => Post) private allPosts;
+
+    function createPost(Post memory post, string memory displayName) public {
         allPosts.push(post);
+        PostToUser[post.postIndex]=displayName;
         userPosts[displayName].push(post);
     }
 
-    function getUserPosts(address userAdd) public view returns (Post[] memory){
-        string memory displayName = mappingPubkeyContract.getDisplayName(userAdd);
+    function getUserPosts(string memory displayName)
+        public
+        view
+        returns (Post[] memory)
+    {
         return userPosts[displayName];
     }
 
-    function getAllPosts() public view returns(Post[] memory){
+    function getAllPosts() public view returns (Post[] memory) {
         return allPosts;
     }
-}
 
-contract Reward{
-    event Transfer(address indexed from, address indexed to, uint amount);
-    
-    function transferFunds(address payable recipient, uint amount) public payable {
-        require(amount > 0, "Amount must be greater than 0");
-        require(address(this).balance >= amount, "Insufficient balance in the account");
-        (bool success, ) = recipient.call{value: amount}("");
-        require(success, "Transfer failed");
-         emit Transfer(msg.sender, recipient, amount);
+    function sendRewardToPost(uint256 postId) public payable {
+        require(msg.value > 0, "Amount must be greater than 0");
+        require(
+            address(msg.sender).balance >= msg.value,
+            "Insufficient balance in the account"
+        );
+        allPosts[postId].Reward += msg.value;
     }
-    receive() external payable {}
-}
 
+    function withdrawReward(string memory displayName,uint256 postId) public payable {
+        require(
+            keccak256(abi.encodePacked(displayName)) == keccak256(abi.encodePacked(PostToUser[postId])),
+            "Display name does not match."
+        );
+        (bool success, ) = payable(msg.sender).call{value: allPosts[postId].Reward}("");
+        require(success, "Transfer failed.");
+        allPosts[postId].Reward = 0;
+    }
+}
